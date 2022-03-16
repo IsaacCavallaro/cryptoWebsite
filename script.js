65df82c095911ee8a56dc6cac6663bee9a0ab12e
isaaccavallaro <isaaccavallaro@gmail.com>
2022-03-16 18:02:18 +1000

    Setting ID's to coins and adding them to variables in script file

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-
+var btc = document.getElementById("bitcoin");
+var eth = document.getElementById("ethereum");
+var doge = document.getElementById("dogecoin");
 
 var settings = {
     "async": true,
